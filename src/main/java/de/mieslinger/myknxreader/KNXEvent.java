8678b2bf6486aa4736088eecbd88d9a0e58001b7
Thomas Mieslinger <thomas.mieslinger@1und1.de>
2020-05-16 09:18:15 +0200

    add some doc on datapoint type size

--- src/main/java/de/mieslinger/myknxreader/KNXEvent.java
+++ src/main/java/de/mieslinger/myknxreader/KNXEvent.java
@@ -69,6 +69,30 @@ public class KNXEvent {
         this.d = e.getDestination();
         this.dp = datapoints.get(e.getDestination());
         this.asdu = e.getASDU();
+        
+    /*    
+     * 1.yyy = boolesch, wie Schalten, Bewegen nach oben/unten, Schritt
+     * 2.yyy = 2 x boolesch, z. B. Schalten + Prioritätssteuerung
+     * 3.yyy = boolesch + vorzeichenloser 3-Bit-Wert, z. B. Auf-/Abdimmen
+     * 4.yyy = Zeichen (8-Bit)
+     * 5.yyy = vorzeichenloser 8-Bit-Wert, wie Dimm-Wert (0..100 %), Jalousienposition (0..100 %)
+     * 6.yyy = 8-Bit-2-Komplement, z. B. %
+     * 7.yyy = 2 x vorzeichenloser 8-Bit-Wert, z. B. Impulszähler
+     * 8.yyy = 2 x 8-Bit-2-Komplement, z. B. %
+     * 9.yyy = 16-Bit-Gleitkommazahl, z. B. Temperatur
+     * 10.yyy = Uhrzeit
+     * 11.yyy = Datum
+     * 12.yyy = 4 x vorzeichenloser 8-Bit-Wert,z. B. Impulszähler
+     * 13.yyy = 4 x 8-Bit-2-Komplement, z. B. Impulszähler
+     * 14.yyy = 32-Bit-Gleitkommazahl, z. B. Temperatur
+     * 15.yyy = Zugangskontrolle
+     * 16.yyy = String -> 14 Zeichen (14 x 8-Bit)
+     * 17.yyy = Szenennummer
+     * 18.yyy = Szenensteuerung
+     * 19.yyy = Uhrzeit + Datum
+     * 20.yyy = 8-Bit-Nummerierung,z. B. HLK-Modus („Automatik“, „Komfort“, „Stand-by“, „Sparen“, „Schutz“)
+     */
+        
         if (dp.getMainNumber() == 1) {
             isBoolean = true;
         }
